01e5d97172a40601511480c84a113927a1a52098
Pratik1968 <47908854+Pratik1968@users.noreply.github.com>
2024-01-11 19:06:44 +0530

    Revert "trying to push project"

--- footlab/app/Error/errorCode.ts
+++
@@ -1,4 +0,0 @@
-export default class errorCode{
-  static AlreadyHaveAccount: string = '23505';
-  static NoCartFound:number = 42703;
-}
--- footlab/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // basePath:"/SignUp"
-}
-
-module.exports = nextConfig
