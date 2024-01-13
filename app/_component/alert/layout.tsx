a35ca2c3cf0a2365d2febe3cff83f23d2d48118c
Pratik1968 <47908854+Pratik1968@users.noreply.github.com>
2024-01-13 14:56:54 +0530

    fixed layout for desktop

--- app/_component/alert/layout.tsx
+++ app/_component/alert/layout.tsx
@@ -2,7 +2,7 @@
 import { ShoppingCart } from "lucide-react";
 export default function AlertBox({title,discription}:{title:string,discription:string}) {
     return (
-      <div className=" fixed bottom-[0px] flex  bg-white w-full gap-3 p-5 shadow">
+      <div className=" fixed bottom-[0px] flex  bg-white w-full gap-3 p-5 shadow lg:w-fit lg:p-5 lg:left-[.7rem] lg:bottom-2">
        
             <div className="">
                 <ShoppingCart className="h-4 w-4" />
